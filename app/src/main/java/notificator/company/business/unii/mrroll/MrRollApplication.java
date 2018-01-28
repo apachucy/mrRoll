223d147344be8fc6e32997f815385a245a06fc08
Arkadiusz Pachucy <pachucek@gmail.com>
2018-01-28 13:48:19 +0100

    Add crashlitics plugin

--- app/src/main/java/notificator/company/business/unii/mrroll/MrRollApplication.java
+++ app/src/main/java/notificator/company/business/unii/mrroll/MrRollApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import notificator.company.business.unii.mrroll.dagger.ApplicationComponent;
 import notificator.company.business.unii.mrroll.dagger.ApplicationModule;
 import notificator.company.business.unii.mrroll.dagger.DaggerApplicationComponent;
@@ -29,6 +31,7 @@ public class MrRollApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
 
         applicationComponent = DaggerApplicationComponent.builder()
