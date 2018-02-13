4a7b909667757637acfa6f083f853f7f83365fa8
Arkadiusz Pachucy <pachucek@gmail.com>
2018-02-13 20:55:47 +0100

    add token instance service

--- app/src/main/java/notificator/company/business/unii/mrroll/service/TokenInstanceService.java
+++ app/src/main/java/notificator/company/business/unii/mrroll/service/TokenInstanceService.java
@@ -9,34 +9,30 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
 import javax.inject.Inject;
 
 import notificator.company.business.unii.mrroll.MrRollApplication;
-import notificator.company.business.unii.mrroll.dagger.ApplicationComponent;
+import notificator.company.business.unii.mrroll.di.ApplicationComponent;
 import notificator.company.business.unii.mrroll.persistance.ConfigurationManager;
 
 
 public class TokenInstanceService extends FirebaseInstanceIdService {
 
     private static final String TAG = TokenInstanceService.class.toString();
-    @Inject
-    ConfigurationManager configurationManager;
+  //  @Inject
+  //  ConfigurationManager configurationManager;
 
     public TokenInstanceService() {
-        injectDependencies(((MrRollApplication) getApplication()).getApplicationComponent());
+  //      injectDependencies(((MrRollApplication) getApplication()).getApplicationComponent());
     }
 
 
     @Override
     public void onTokenRefresh() {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        String previousToken = configurationManager.getCloudToken();
+  //      String previousToken = configurationManager.getCloudToken();
 
         Log.v(TAG, "Obtained refreshed Token: " + refreshedToken);
-        configurationManager.setCloudToken(refreshedToken);
+  //      configurationManager.setCloudToken(refreshedToken);
 
         //TODO: send message to server side
     }
 
-
-    protected void injectDependencies(@NonNull ApplicationComponent applicationComponent) {
-        applicationComponent.inject(this);
-    }
 }
